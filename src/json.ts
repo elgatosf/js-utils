a1215bd8fda8ef27e92b1446959c43eb0e0490c3
Richard Herman <geekyeggo@users.noreply.github.com>
2025-10-28 12:10:47 +0000

    feat: expand JSON type to support more serializable formats

--- src/json.ts
+++ src/json.ts
@@ -1,16 +1,32 @@
 /**
- * JSON object.
+ * Object that can be serialized/deserialized to/from JSON.
  */
 export type JsonObject = {
 	[key: string]: JsonValue;
 };
 
 /**
- * JSON primitive value.
+ * Primitive value that can be serialized/deserialized to/from JSON.
  */
 export type JsonPrimitive = boolean | number | string | null | undefined;
 
 /**
- * JSON value.
+ * Data that can be serialized/deserialized to/from JSON.
  */
 export type JsonValue = JsonObject | JsonPrimitive | JsonValue[];
+
+/**
+ * Object that can be serialized to JSON.
+ */
+export type JsonSerializableObject =
+	| {
+			[key: string]: JsonSerializableObject;
+	  }
+	| {
+			toJSON(): JsonSerializableValue[] | JsonValue;
+	  };
+
+/**
+ * Data that can be serialized to JSON.
+ */
+export type JsonSerializableValue = JsonSerializableObject | JsonSerializableValue[] | JsonValue;
